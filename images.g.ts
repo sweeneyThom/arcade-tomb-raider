2b601a076f262518dec4b0c9828458aed1268a9e
Thomas Sweeney <69155222+sweeneyThom@users.noreply.github.com>
2021-05-07 13:06:26 -0400

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -80,17 +80,6 @@ c b c b c b c 9
 8 9 9 8 8 9 9 9 
 9 9 9 9 9 8 9 9 
 `;
-            case "image9":
-            case "player_right":return img`
-. . . . . 7 . . 
-. . 7 . 7 7 . . 
-. . 7 7 7 . . . 
-. . 4 4 9 . . . 
-. . 2 4 4 . . . 
-. . 5 2 5 . . . 
-. . 5 5 2 . . . 
-. . 9 . 9 . . . 
-`;
             case "image5":
             case "myTile":return img`
 9 8 9 9 9 9 9 8 
@@ -132,6 +121,88 @@ c b c b c b c 9
 8 9 8 9 9 8 9 6 
 8 9 9 8 8 9 9 9 
 9 9 9 9 9 8 9 9 
+`;
+            case "image9":
+            case "player_right":return img`
+. . . . . 7 . . 
+. . 7 . 7 7 . . 
+. . 7 7 7 . . . 
+. . 4 4 9 . . . 
+. . 2 4 4 . . . 
+. . 5 2 5 . . . 
+. . 5 5 2 . . . 
+. . 9 . 9 . . . 
+`;
+            case "Tn3t|cixc-+UpB":
+            case "spike_falling":return img`
+e d d d d d e . 
+e d d d d d e . 
+. e d d d e . . 
+. e d d d e . . 
+. . e d e . . . 
+. . e d e . . . 
+. . . e . . . . 
+. . . e . . . . 
+`;
+            case "image13":
+            case "myTile12":return img`
+. . . . . . . . 
+. . 2 2 2 2 . . 
+. . 2 . . 2 . . 
+. . 2 . 2 2 . . 
+. . 2 2 2 . . . 
+. . 2 . 2 2 . . 
+. . 2 2 . 2 . . 
+. . . 2 2 2 . . 
+`;
+            case "image14":
+            case "myTile17":return img`
+9 8 c c d d 9 8 
+9 c d d d d d 8 
+c d c d d d d d 
+d c d d d d d d 
+d d d d d d d d 
+d d d c d d d d 
+8 d d d d c d 9 
+9 9 d d d d 9 9 
+`;
+            case "image17":
+            case "player_item_get":return img`
+. . 7 7 7 . . . 
+. . a a a . . . 
+. 4 7 7 7 4 . . 
+. 4 9 4 9 4 . . 
+. . 2 4 4 . . . 
+. . 5 2 5 . . . 
+. . 5 5 2 . . . 
+. . 9 . 9 . . . 
+`;
+            case "image18":
+            case "myTile2":return img`
+9 8 9 9 9 9 9 8 
+9 8 8 9 9 9 8 8 
+9 9 8 9 9 8 9 9 
+9 8 9 9 9 8 9 9 
+9 8 9 8 9 9 8 9 
+8 9 8 9 9 8 9 9 
+8 9 9 8 8 9 9 9 
+9 9 9 9 9 8 9 9 
+`;
+            case "image16":
+            case "arrow_right":return img`
+. . . . . . 5 
+. 9 . . 5 5 . 
+9 6 6 6 6 6 . 
+. 9 . . 5 5 . 
+. . . . . . 5 
+`;
+            case "image15":
+            case "arrow_left":return img`
+5 . . . . . . 
+. 5 5 . . 9 . 
+. 6 6 6 6 6 9 
+. 5 5 . . 9 . 
+5 . . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -15,6 +15,8 @@ function loadMap () {
         sprite.setKind(SpriteKind._TileSprite)
     }
     tiles.destroySpritesOfKind(SpriteKind._TileSprite)
+    tiles.destroySpritesOfKind(SpriteKind.Projectile)
+    tiles.destroySpritesOfKind(SpriteKind.Gem)
     if (currentLocationRow == 0) {
         if (currentLocationCol == 0) {
         	
@@ -25,13 +27,13 @@ function loadMap () {
         } else if (currentLocationCol == 3) {
         	
         } else if (currentLocationCol == 4) {
-        	
+            tiles.setSmallTilemap(tilemap`level18`)
         } else if (currentLocationCol == 5) {
             tiles.setSmallTilemap(tilemap`level5`)
         } else if (currentLocationCol == 6) {
             tiles.setSmallTilemap(tilemap`level6`)
         } else if (currentLocationCol == 7) {
-        	
+            tiles.setSmallTilemap(tilemap`level13`)
         } else {
         	
         }
@@ -45,15 +47,35 @@ function loadMap () {
         } else if (currentLocationCol == 3) {
         	
         } else if (currentLocationCol == 4) {
-        	
+            tiles.setSmallTilemap(tilemap`level17`)
         } else if (currentLocationCol == 5) {
-        	
+            tiles.setSmallTilemap(tilemap`level15`)
         } else if (currentLocationCol == 6) {
             tiles.setSmallTilemap(tilemap`level11`)
         } else if (currentLocationCol == 7) {
+            tiles.setSmallTilemap(tilemap`level24`)
+        } else {
+            tiles.setSmallTilemap(tilemap`level25`)
+        }
+    } else if (currentLocationRow == 2) {
+        if (currentLocationCol == 0) {
+        	
+        } else if (currentLocationCol == 1) {
+        	
+        } else if (currentLocationCol == 2) {
+        	
+        } else if (currentLocationCol == 3) {
+        	
+        } else if (currentLocationCol == 4) {
+        	
+        } else if (currentLocationCol == 5) {
+            tiles.setSmallTilemap(tilemap`level19`)
+        } else if (currentLocationCol == 6) {
+        	
+        } else if (currentLocationCol == 7) {
         	
         } else {
-        	
+            tiles.setSmallTilemap(tilemap`level26`)
         }
     } else {
     	
@@ -61,25 +83,26 @@ function loadMap () {
     scene.centerCameraAt(88, 68)
     tiles.coverAllTiles(assets.tile`myTile10`, assets.tile`myTile`)
     tiles.coverAllTiles(assets.tile`myTile7`, assets.tile`myTile6`)
+    tiles.coverAllTiles(assets.tile`myTile12`, assets.tile`myTile`)
     for (let value of tiles.getTilesByType(assets.tile`myTile13`)) {
         gem = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Gem)
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, SpriteKind.Gem)
         tiles.placeOnTile(gem, value)
         animation.runImageAnimation(
         gem,
@@ -210,10 +233,28 @@ function loadMap () {
         gem.x += 3
         tiles.setTileAt(value, assets.tile`myTile`)
     }
+    foundString = "" + currentLocationCol + "," + currentLocationRow
+    for (let value2 of gemsFound) {
+        if (value2 == foundString) {
+            tiles.destroySpritesOfKind(SpriteKind.Gem)
+        }
+    }
+    if (bouldersFallen) {
+        tiles.coverAllTiles(assets.tile`myTile12`, assets.tile`myTile17`)
+        for (let value3 of tiles.getTilesByType(assets.tile`myTile12`)) {
+            tiles.setWallAt(value3, true)
+        }
+    }
+    if (collapseCracks) {
+        for (let value3 of tiles.getTilesByType(assets.tile`myTile16`)) {
+            tiles.setTileAt(value3, assets.tile`myTile`)
+            tiles.setWallAt(value3, false)
+        }
+    }
     if (enteringFrom == CollisionDirection.Right) {
         thePlayer.x = 152
     } else if (enteringFrom == CollisionDirection.Bottom) {
-        thePlayer.y = 152
+        thePlayer.y = 118
         doAJump(thePlayer, jumpHeight)
     } else if (enteringFrom == CollisionDirection.Left) {
         thePlayer.x = 24
@@ -224,34 +265,64 @@ function loadMap () {
 sprites.onDestroyed(SpriteKind.Crumbler, function (sprite) {
     tiles.setWallAt(tiles.locationOfSprite(sprite), false)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Gem, function (sprite, otherSprite) {
+    otherSprite.setKind(SpriteKind.Food)
+    timerRunning = false
+    character.setCharacterAnimationsEnabled(thePlayer, false)
+    thePlayer.setImage(assets.image`player_item_get`)
+    canMove = false
+    animation.stopAnimation(animation.AnimationTypes.All, otherSprite)
+    otherSprite.bottom = thePlayer.top
+    otherSprite.x = thePlayer.x
+    gemsFound.push("" + currentLocationCol + "," + currentLocationRow)
+    music.playMelody("D E F G - - - - ", 120)
+    pause(1000)
+    otherSprite.destroy()
+    character.setCharacterAnimationsEnabled(thePlayer, true)
+    thePlayer.setImage(assets.image`player_right`)
+    timerRunning = true
+    canMove = true
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (thePlayer.isHittingTile(CollisionDirection.Bottom)) {
-        doAJump(thePlayer, jumpHeight)
+    doAJump(thePlayer, jumpHeight)
+    if (canMove) {
+        if (thePlayer.isHittingTile(CollisionDirection.Bottom)) {
+        	
+        }
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
-    doAJump(sprite, 20)
+    takeDamage()
+})
+sprites.onDestroyed(SpriteKind.Spike, function (sprite) {
+    tiles.setTileAt(tiles.locationOfSprite(sprite), assets.tile`myTile9`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile18`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`myTile20`)
+    fallingBlock = sprites.create(assets.image`spike_falling`, SpriteKind.Projectile)
+    fallingBlock.y = thePlayer.y - 15
+    if (thePlayer.vx < 0) {
+        fallingBlock.x = thePlayer.x - 15
+    } else {
+        fallingBlock.x = thePlayer.x + 15
+    }
+    fallingBlock.setFlag(SpriteFlag.GhostThroughWalls, true)
+    fallingBlock.vy = 100
+})
+function takeDamage () {
+    doAJump(thePlayer, 20)
     life += -1
     thePlayer.setKind(SpriteKind.Food)
     setTimeout(function(){
         thePlayer.setKind(SpriteKind.Player)
     }, 300)
-})
-sprites.onDestroyed(SpriteKind.Spike, function (sprite) {
-    tiles.setTileAt(tiles.locationOfSprite(sprite), assets.tile`myTile9`)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    takeDamage()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
     tiles.setTileAt(location, assets.tile`myTile`)
-    tileSprite = sprites.create(img`
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . b b b b b b . 
-        `, SpriteKind.Spike)
+    tileSprite = sprites.create(assets.image`myTile2`, SpriteKind.Spike)
     tiles.placeOnTile(tileSprite, location)
     animation.runImageAnimation(
     tileSprite,
@@ -365,27 +436,41 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile10`, function (sprite, 
 let arrow2: Sprite = null
 let arrow: Sprite = null
 let newCover: Sprite = null
+let bottomTile: tiles.Location = null
 let tileSprite: Sprite = null
+let fallingBlock: Sprite = null
+let foundString = ""
 let gem: Sprite = null
 let currentLocationRow = 0
 let currentLocationCol = 0
+let gemsFound: string[] = []
+let collapseCracks = false
+let canMove = false
+let timerRunning = false
 let GRAVITY = 0
 let jumpHeight = 0
-let enteringFrom: CollisionDirection = null
-let timerColor:color = null
-let str:string = null
-let thePlayer: Sprite = null
+let bouldersFallen = false
 let isTimerOn = false
-let bottomTile: tiles.Location = null
+let thePlayer: Sprite = null
+let str:string = null
+let timerColor:color = null
+let enteringFrom: CollisionDirection = null
+function addTimerEvent(seconds:number, event:() => void){
+    control.onEvent(TIMER_EVENT_ID, seconds, event)
+}
+let TIMER_EVENT_ID = 1008
 let life = 5
 jumpHeight = 25
 GRAVITY = 400
-let timerRunning = true
+timerRunning = true
+canMove = true
+collapseCracks = true
 let minutesLeft = 4
 let secondsLeft = 59
 thePlayer = sprites.create(assets.image`player`, SpriteKind.Player)
 controller.moveSprite(thePlayer, 75, 0)
 thePlayer.ay = GRAVITY
+gemsFound = ["5,0"]
 currentLocationCol = 5
 currentLocationRow = 0
 character.loopFrames(
@@ -396,19 +481,16 @@ character.rule(Predicate.FacingRight)
 )
 character.loopFrames(
 thePlayer,
-[img`
-    . . 7 . . . . . 
-    . . 7 7 . 7 . . 
-    . . . 7 7 7 . . 
-    . . . 9 4 4 . . 
-    . . . 4 4 2 . . 
-    . . . 5 2 5 . . 
-    . . . 2 5 5 . . 
-    . . . 9 . 9 . . 
-    `],
+[],
 500,
 character.rule(Predicate.FacingLeft)
 )
+addTimerEvent(120, function(){
+    bouldersFallen = true
+})
+addTimerEvent(180, function(){
+    collapseCracks = true
+})
 tiles.setSmallTilemap(tilemap`level2`)
 tiles.placeOnTile(thePlayer, tiles.getTileLocation(4, 13))
 let passableTiles = [assets.tile`myTile1`, assets.tile`myTile2`, assets.tile`myTile3`]
@@ -435,10 +517,19 @@ scene.createRenderable(5, function(target: Image, camera: scene.Camera) {
     }
 })
 game.onUpdate(function () {
-    if (thePlayer.tileKindAt(TileDirection.Center, assets.tile`myTile2`) || thePlayer.tileKindAt(TileDirection.Center, assets.tile`myTile0`)) {
-        controller.moveSprite(thePlayer, 75, 75)
+    if (life <= 0) {
+        game.over(false)
+    }
+})
+game.onUpdate(function () {
+    if (canMove) {
+        if (thePlayer.tileKindAt(TileDirection.Center, assets.tile`myTile2`) || thePlayer.tileKindAt(TileDirection.Center, assets.tile`myTile0`)) {
+            controller.moveSprite(thePlayer, 75, 75)
+        } else {
+            controller.moveSprite(thePlayer, 75, 0)
+        }
     } else {
-        controller.moveSprite(thePlayer, 75, 0)
+        controller.moveSprite(thePlayer, 0, 0)
     }
 })
 game.onUpdate(function () {
@@ -446,80 +537,23 @@ game.onUpdate(function () {
     if (tiles.tileIs(bottomTile, assets.tile`myTile7`)) {
         newCover = sprites.create(assets.image`myTile`, SpriteKind._TileSprite)
         tiles.placeOnTile(newCover, bottomTile)
+        newCover.z = -1
         tileSprite = sprites.create(assets.tile`myTile6`, SpriteKind.Crumbler)
         animation.runImageAnimation(
         tileSprite,
-        [img`
-            7 7 7 7 7 7 7 9 . . 
-            9 7 7 7 7 7 9 7 . . 
-            7 9 7 7 7 9 7 7 . . 
-            9 7 9 7 9 7 7 7 . . 
-            7 9 7 9 7 7 7 7 . . 
-            9 7 9 9 9 7 7 7 . . 
-            7 9 9 9 9 9 7 7 . . 
-            9 9 9 9 9 9 9 7 . . 
-            . . . . . . . . . . 
-            . . . . . . . . . . 
-            `,img`
-            . . . . . . . . . . 
-            . 7 7 7 7 7 7 7 9 . 
-            . 9 7 7 7 7 7 9 7 . 
-            . 7 9 7 7 7 9 7 7 . 
-            . 9 7 9 7 9 7 7 7 . 
-            . 7 9 7 9 7 7 7 7 . 
-            . 9 7 9 9 9 7 7 7 . 
-            . 7 9 9 9 9 9 7 7 . 
-            . 9 9 9 9 9 9 9 7 . 
-            . . . . . . . . . . 
-            `,img`
-            . . . . . . . . . . 
-            . . . . . . . . . . 
-            . . 7 7 7 7 7 7 7 9 
-            . . 9 7 7 7 7 7 9 7 
-            . . 7 9 7 7 7 9 7 7 
-            . . 9 7 9 7 9 7 7 7 
-            . . 7 9 7 9 7 7 7 7 
-            . . 9 7 9 9 9 7 7 7 
-            . . 7 9 9 9 9 9 7 7 
-            . . 9 9 9 9 9 9 9 7 
-            `,img`
-            . . . . . . . . . . 
-            7 7 7 7 7 7 7 9 . . 
-            9 7 7 7 7 7 9 7 . . 
-            7 9 7 7 7 9 7 7 . . 
-            9 7 9 7 9 7 7 7 . . 
-            7 9 7 9 7 7 7 7 . . 
-            9 7 9 9 9 7 7 7 . . 
-            7 9 9 9 9 9 7 7 . . 
-            9 9 9 9 9 9 9 7 . . 
-            . . . . . . . . . . 
-            `,img`
-            . 7 7 7 7 7 7 7 9 . 
-            . 9 7 7 7 7 7 9 7 . 
-            . 7 9 7 7 7 9 7 7 . 
-            . 9 7 9 7 9 7 7 7 . 
-            . 7 9 7 9 7 7 7 7 . 
-            . 9 7 9 9 9 7 7 7 . 
-            . 7 9 9 9 9 9 7 7 . 
-            . 9 9 9 9 9 9 9 7 . 
-            . . . . . . . . . . 
-            . . . . . . . . . . 
-            `],
+        [],
         100,
         true
         )
         tileSprite.lifespan = 1000
         tiles.placeOnTile(tileSprite, bottomTile)
         tiles.setTileAt(bottomTile, assets.tile`myTile`)
-        setTimeout(function(){
-            tiles.setWallAt(bottomTile, false)
-        }, 1000)
     }
 })
 game.onUpdate(function () {
-    for (let value2 of passableTiles) {
-        for (let loc of tiles.getTilesByType(value2)) {
-            if (thePlayer.tileKindAt(TileDirection.Bottom, value2) && !(controller.down.isPressed())) {
+    for (let value22 of passableTiles) {
+        for (let loc of tiles.getTilesByType(value22)) {
+            if (thePlayer.tileKindAt(TileDirection.Bottom, value22) && !(controller.down.isPressed()) || !(canMove)) {
                 tiles.setWallAt(loc, true)
             } else if (controller.down.isPressed()) {
                 tiles.setWallAt(loc, false)
@@ -537,7 +571,7 @@ game.onUpdateInterval(1000, function () {
         } else if (minutesLeft == 0 && (secondsLeft < 30 && secondsLeft % 2 == 0)) {
             music.playTone(294, music.beat(BeatFraction.Eighth))
         }
-        if (secondsLeft <= 0) {
+        if (secondsLeft < 0) {
             minutesLeft += -1
             secondsLeft = 59
             music.playTone(440, music.beat(BeatFraction.Eighth))
@@ -545,77 +579,20 @@ game.onUpdateInterval(1000, function () {
                 game.over(false)
             }
         }
+        control.raiseEvent(TIMER_EVENT_ID, secondsLeft + (minutesLeft * 60))
     }
 })
 game.onUpdateInterval(1500, function () {
-    for (let value3 of tiles.getTilesByType(assets.tile`myTile14`)) {
-        arrow = sprites.create(img`
-            . . . . . . . 6 
-            . 9 . . . 6 6 . 
-            9 7 7 7 7 7 7 . 
-            . 9 . . . 6 6 . 
-            . . . . . . . 6 
-            `, SpriteKind.Projectile)
-        tiles.placeOnTile(arrow, value3)
+    for (let value32 of tiles.getTilesByType(assets.tile`myTile14`)) {
+        arrow = sprites.create(assets.image`arrow_right`, SpriteKind.Projectile)
+        tiles.placeOnTile(arrow, value32)
         arrow.vx = -100
+        arrow.setFlag(SpriteFlag.DestroyOnWall, true)
     }
     for (let value4 of tiles.getTilesByType(assets.tile`myTile15`)) {
-        arrow2 = sprites.create(img`
-            6 . . . . . . . 
-            . 6 6 . . . 9 . 
-            . 7 7 7 7 7 7 9 
-            . 6 6 . . . 9 . 
-            6 . . . . . . . 
-            `, SpriteKind.Projectile)
+        arrow2 = sprites.create(assets.image`arrow_left`, SpriteKind.Projectile)
         tiles.placeOnTile(arrow2, value4)
         arrow2.vx = 100
-    }
-})
-forever(function () {
-    if (isTimerOn) {
-        music.playTone(440, music.beat(BeatFraction.Eighth))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(440, music.beat(BeatFraction.Eighth))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(370, music.beat(BeatFraction.Whole))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(349, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(311, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(294, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(349, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(370, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(370, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(392, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(392, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(440, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(466, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(440, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(349, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(330, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(392, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
-        music.playTone(415, music.beat(BeatFraction.Half))
-        music.rest(music.beat(BeatFraction.Eighth))
+        arrow2.setFlag(SpriteFlag.DestroyOnWall, true)
     }
 })
